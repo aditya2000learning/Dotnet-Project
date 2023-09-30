5336acb9155b4aac3e857f113dce16a5adef2e21
Aditya <aditya2000learning@gmail.com>
2023-09-30 22:21:58 +0530

    first commit

---
+++ DotNetApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNetApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
